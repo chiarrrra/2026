490c027436306771fde58ab033744a9b31723fa0
Andreas Gysin <andreas@ertdfgcvb.ch>
2026-05-04 12:09:45 +0200

    Create sketch.js

---
+++ esempi/es01_starter/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+	createCanvas(800, 600)
+	background(200, 200, 200)
+}
+
+function draw() {
+	if(mouseIsPressed) {
+		fill(random(256), random(256), random(256))
+		circle(mouseX, mouseY, 80)
+		circle(width - mouseX, mouseY, 80)
+	}
+}
+
+function keyPressed() {
+	if (key == 's') {
+		save("maschera_" + (new Date()).getTime() + ".png")
+	} else if (key == 'n') {
+		background(200, 200, 200)
+	} else if (key == 'r') {
+		background(random(256), random(256), random(256))
+	}
+}
+
