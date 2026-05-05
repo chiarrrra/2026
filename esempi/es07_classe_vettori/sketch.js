1155cad66079065eebf72e032792214596895f9c
Andreas Gysin <andreas@ertdfgcvb.ch>
2026-05-05 12:16:21 +0200

    Update sketch.js

--- esempi/es07_classe_vettori/sketch.js
+++ esempi/es07_classe_vettori/sketch.js
@@ -3,7 +3,31 @@ function setup() {
 }
 
 function draw() {
-	background(230)
+	background(220)
+
+	const vettoreCentro = new Vec2(width/2, height/2)
+	vettoreCentro.disegna("red")
+
+	const vettoreMouse = new Vec2(mouseX, mouseY)
+	//vettoreMouse.disegna("blue")
+
+	// vettoreMouse - vettoreCentro
+	const vettoreCM = vettoreMouse.sottrai(vettoreCentro)
+	vettoreCM.disegna("green", vettoreCentro)
+
+	const vettoreCMInverso = new Vec2(-vettoreCM.x, -vettoreCM.y)
+	vettoreCMInverso.disegna("orange", vettoreCentro)
+
+	const vettoreCMSinistra = new Vec2(vettoreCM.y, -vettoreCM.x)
+	vettoreCMSinistra.disegna("orange", vettoreCentro)
+
+	const vettoreCMDestra = new Vec2(-vettoreCM.y, vettoreCM.x)
+	vettoreCMDestra.disegna("orange", vettoreCentro)
+
+
+	const vettore100 = vettoreCM.normalizza().moltiplica(100)
+	vettore100.disegna("brown", vettoreCentro)
+
 }
 
 
