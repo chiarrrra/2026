bec749d29056892d5ecc8c5b3ccf1b5d117aa79e
Andreas Gysin <andreas@ertdfgcvb.ch>
2026-05-05 16:03:50 +0200

    Create sketch.js

---
+++ esempi/es11_dyna/sketch.js
@@ -0,0 +1,44 @@
+/*
+Tentativo di implementazione di “DynaDraw” di Paul Haeberli
+https://www.graficaobscura.com/dyna/index.html
+
+*/
+
+let posizione
+let forza
+const coda = []
+
+function setup() {
+	createCanvas(800, 600)
+	posizione = new Vec2(width/2, height/2)
+	forza = new Vec2(0, 0)
+}
+
+function draw() {
+
+	const m = new Vec2(mouseX, mouseY)
+
+	const delta = m.sottrai(posizione).moltiplica(0.04)
+	forza = forza.somma(delta)
+	forza = forza.moltiplica(0.95)
+	posizione = posizione.somma(forza)
+
+	coda.push(new Vec2(posizione.x, posizione.y))
+
+
+	while(coda.length > 100) {
+		coda.shift()
+	}
+
+	background(220)
+	noFill()
+	strokeWeight(4)
+	beginShape()
+	for (let i=0; i<coda.length; i++) {
+		const v = coda[i]
+		vertex(v.x, v.y)
+	}
+	endShape()
+}
+
+
