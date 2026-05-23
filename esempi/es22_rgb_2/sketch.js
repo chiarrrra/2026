47a5c301c5d0d4343d4de7ec0d00995cf01f0a1d
Andreas Gysin <andreas@ertdfgcvb.ch>
2026-05-23 11:50:45 +0200

    Update sketch.js

--- esempi/es22_rgb_2/sketch.js
+++ esempi/es22_rgb_2/sketch.js
@@ -1,26 +1,37 @@
 
 let immagineA
 let immagineB
+
 function preload() {
-	immagineA = loadImage("foglie.jpg")
-	immagineB = loadImage("gatto.jpg")
+	immagineA = loadImage("gatto.jpg")
+	immagineB = loadImage("foglie.jpg")
 }
 
 function setup() {
 	createCanvas(400, 400)
 	immagineA.loadPixels()
 	immagineB.loadPixels()
-
 }
+
 function draw() {
 	background(0)
 
+
+	const sfum = mouseX / width
+
 	for (let y = 0; y < height; y++) {
 		for (let x = 0; x < height; x++) {
-			const col = pixel(immagineA, x, y)
-			set(x, y, color(col.r, col.g, col.b))
+			const col1 = pixel(immagineA, x, y)
+			const col2 = pixel(immagineB, x-100, y)
+
+			const r = col2.r * sfum + col1.r * (1 - sfum)
+			const g = col2.g * sfum + col1.g * (1 - sfum)
+			const b = col2.b * sfum + col1.b * (1 - sfum)
+
+			set(x, y, color(r, g, b))
 		}
 	}
+
 	updatePixels()
 }
 
